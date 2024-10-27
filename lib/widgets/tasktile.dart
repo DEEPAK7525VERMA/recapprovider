80a2456ecae268f99bd85a0d5a46c34615384e25
Deepak Verma <fourarmskilltotal@gmail.com>
2024-10-27 20:25:07 +0530

    canelo2

--- lib/widgets/tasktile.dart
+++ lib/widgets/tasktile.dart
@@ -1,15 +1,46 @@
 //tasktile.dart
 import "package:flutter/material.dart";
-class TaskTile extends StatelessWidget {
+class TaskTile extends StatefulWidget {
  
+  @override
+  State<TaskTile> createState() => _TaskTileState();
+}
+
+class _TaskTileState extends State<TaskTile> {
+  bool ischecked=false;
+  void changecheckbox(bool? newvalue){
+     { 
+    setState((){
+     ischecked=newvalue??false;
+      });
+     };
+  }
 
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      title:Text("this is a task"),
+      title:Text("this is a task",
+      style:TextStyle(
+        decoration: ischecked?TextDecoration.lineThrough:TextDecoration.none,
+      ),),
        trailing:
-      Checkbox(value:false, onChanged: (bool? value) {  },
-      ),
+      TaskCheckBox(ischecked,changecheckbox),
+    );
+  }
+}
+
+class TaskCheckBox extends StatelessWidget {
+  final bool checkboxState;
+  final Function(bool?) togglecheckbox;
+  TaskCheckBox(this.checkboxState,this.togglecheckbox);
+
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(
+      activeColor: Colors.lightBlueAccent,
+      value:checkboxState, 
+      onChanged:togglecheckbox,
+    
     );
   }
 }
